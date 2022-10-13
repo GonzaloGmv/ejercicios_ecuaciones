ac0bba38557c47df101343838858e32f630e7800
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-10-13 19:35:47 +0200

    ec1

--- ecuaciones.py
+++
@@ -1,9 +0,0 @@
-import sympy
-
-x = sympy.Symbol('x')
-y = sympy.Function('y')
-
-f1 = ((x**2)*y(x)-y(x))/(y(x)-1)
-ics = {y(3):-1}
-solucion = sympy.dsolve(y(x).diff(x)-f1,ics=ics)
-sympy.pprint(solucion)
---
+++ ecuaciones/ec1.py
@@ -0,0 +1,11 @@
+import sympy
+
+def ecuacion1():
+    x = sympy.Symbol('x')
+    y = sympy.Function('y')
+
+    f1 = ((x**2)*y(x)-y(x))/(y(x)-1)
+    ics = {y(3):-1}
+    solucion = sympy.dsolve(y(x).diff(x)-f1,ics=ics)
+    print("Solucion a la ecuacion 1: \n")
+    sympy.pprint(solucion)
