d367bb58ef96384bd98552f6a7dfb3659b0c3973
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-10-13 19:30:52 +0200

    ec1

--- ecuaciones.py
+++ ecuaciones.py
@@ -4,5 +4,6 @@ x = sympy.Symbol('x')
 y = sympy.Function('y')
 
 f1 = ((x**2)*y(x)-y(x))/(y(x)-1)
-solucion = sympy.dsolve(y(x).diff(x)-f1)
+ics = {y(3):-1}
+solucion = sympy.dsolve(y(x).diff(x)-f1,ics=ics)
 sympy.pprint(solucion)
